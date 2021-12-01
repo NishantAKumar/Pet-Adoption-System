de32af3d17eb2f0e9f80430f76b446cab54d77a1
Nishant <nishi.ak77@gmail.com>
2021-12-01 20:17:08 +0530

    Disembodied DRF and reverted to MVT + User model extended using AbstractUser

--- system/core/models.py
+++ system/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    area = models.CharField(max_length=500)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
--- system/core/views.py
+++ system/core/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.http.response import JsonResponse
-from rest_framework import status
-from rest_framework.decorators import api_view
 from .models import * 
 
 # Create your views here.
