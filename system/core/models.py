d4e086006741392e30a15caad00acc51a7f80cd7
Meith Navlakha <meithnavlakha@gmail.com>
2021-12-02 13:16:51 +0530

    Transaction Model

--- system/core/models.py
+++ system/core/models.py
@@ -8,3 +8,12 @@ class User(AbstractUser):
     city = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
     phone = models.CharField(max_length=10)
+    
+    
+class Transaction:
+    id:int
+    donor_id:int
+    img: str
+    desc:str
+    fk_Rid: int
+
