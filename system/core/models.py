fe337b035d5f9b905554c5d662edb3dd542b6c4f
Meith Navlakha <meithnavlakha@gmail.com>
2021-12-04 08:41:21 +0530

    Transaction Model Commented

--- system/core/models.py
+++ system/core/models.py
@@ -8,19 +8,18 @@ class User(AbstractUser):
     city = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
     phone = models.CharField(max_length=10)
-    
-    
-class Transaction(models.Model): # Follow the models.field format
-    # I've done the basic thing because I had to verify my part works
-    donor_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    # id:int
-    # donor_id:int
-    # img: str
-    # desc:str
-    # fk_Rid: int
+   
+# class Transaction(models.Model):
+
+#     name=models.CharField(max_length=100)
+#     desc= models.TextField()
+#     img = models.ImageField(upload_to='pics')
+#     donor_id = models.ForeignKey(User, on_delete=models.CASCADE)
+#     accepted_req_id = models.ForeignKey(Request, on_delete=models.CASCADE)
 
 
 class Request(models.Model):
     transaction_id = models.ForeignKey(Transaction, on_delete=models.CASCADE)
     reason = models.TextField()
     applicant_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
